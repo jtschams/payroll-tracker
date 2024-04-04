b6d5fdab8b80d10cb9b22d7632ca78bd40c0f2ba
jtschams <jtschams@yahoo.com>
2024-04-03 23:27:32 -0500

    added random employee drawing

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -35,7 +35,8 @@ const displayAverageSalary = function(employeesArray) {
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
-  // TODO: Select and display a random employee
+  const winNum = Math.floor(Math.random() * employeesArray.length)
+  console.log(`Congratulations to ${employeesArray[winNum].firstName} ${employeesArray[winNum].lastName}, our random drawing winner!`)
 }
 
 /*
